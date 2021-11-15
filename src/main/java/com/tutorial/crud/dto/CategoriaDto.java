5a1614fa87db92875bbe59b59c4c9c78fb89c1ba
isaacher <johanisaacha@ufps.edu.co>
2021-11-15 14:36:49 -0500

    Modulo Categoria

---
+++ src/main/java/com/tutorial/crud/dto/CategoriaDto.java
@@ -0,0 +1,31 @@
+package com.tutorial.crud.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+public class CategoriaDto {
+    @NotBlank
+    private String nombre;
+
+    public CategoriaDto(){
+
+    }
+
+    public CategoriaDto (@NotBlank String nombre) {
+        this.nombre = nombre;
+    }
+
+      public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoDto{" +
+                "nombre='" + nombre + '}';
+    }
+}
